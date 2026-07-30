87a58bc976cf60c1635ce63734ba61cfe3f07c1d
Curtis Teng <curdiskirby@gmail.com>
2026-07-30 12:10:36 -0700

    Added comments to Independent Tables Script
    Added comments to Independent Tables script as loose guidance based on what I currently know we would most likely have. (Probably prone to change as we might end up making more dependent tables than currently shown in the ERD)

--- Valorant_IndependentTables.sql
+++ Valorant_IndependentTables.sql
@@ -6,37 +6,44 @@ DROP TABLE IF EXISTS Weapon;
 DROP TABLE IF EXISTS Gamemode;
 DROP TABLE IF EXISTS Map;
 
+-- Would be used in Player table
 CREATE TABLE Region (
 	RegionID INTEGER PRIMARY KEY AUTOINCREMENT,
 	RegionName VARCHAR(50) NOT NULL UNIQUE
 );
 
+-- Would be used in Player table and potentialyl MatchPlayer table
 CREATE TABLE Rank (
 	RankID INTEGER PRIMARY KEY AUTOINCREMENT,
 	RankName VARCHAR(50) NOT NULL UNIQUE
 );
 
+-- Would be used in Agent table
 CREATE TABLE Role (
 	RoleID INTEGER PRIMARY KEY AUTOINCREMENT,
 	RoleName VARCHAR(50) NOT NULL UNIQUE
 );
 
+-- Would be used in Agent table and PlayerAbilityLoadout table
 CREATE TABLE Ability (
 	AbilityID INTEGER PRIMARY KEY AUTOINCREMENT,
 	AbilityName VARCHAR(50) NOT NULL UNIQUE
 );
 
+-- Would be used in MatchPlayer table and PlayerAbilityLoadout table
 CREATE TABLE Weapon (
 	WeaponID INTEGER PRIMARY KEY AUTOINCREMENT,
 	WeaponName VARCHAR(50) NOT NULL UNIQUE,
 	WeaponType VARCHAR(50)
 );
 
+-- Would be used in Match table
 CREATE TABLE Gamemode (
 	GamemodeID INTEGER PRIMARY KEY AUTOINCREMENT,
 	GamemodeName VARCHAR(50) NOT NULL UNIQUE
 );
 
+-- Would be used in Match table
 CREATE TABLE Map (
 	MapID INTEGER PRIMARY KEY AUTOINCREMENT,
 	MapName VARCHAR(50) NOT NULL UNIQUE
